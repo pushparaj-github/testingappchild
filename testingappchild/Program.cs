54df7e8c22d8f2e0a3b1a0593b3ce55928831f4f
Pushparaj <pushparaj05@gmail.com>
2024-09-16 22:14:03 +0530

    Add project files.

---
+++ testingappchild/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OidcAuth;
+
+Console.WriteLine("Hello, World!");
+var retval = GetOidcAuth.GetUserDetails();
+Console.WriteLine(retval.ToString());
